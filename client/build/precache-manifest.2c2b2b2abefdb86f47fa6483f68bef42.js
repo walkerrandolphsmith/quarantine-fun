1bba84aa4c6e0a4188fc35860cd89d0afb10366c
walker smith <wsmith@collab.net>
2020-04-08 08:31:58 -0400

    prod

---
+++ client/build/precache-manifest.2c2b2b2abefdb86f47fa6483f68bef42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2131e43b3e1417f68653dc8048cb8720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b550c4368917400ba70",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "1fb4588ded82ce003b8a",
+    "url": "/static/js/2.6d46b0e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d46b0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b550c4368917400ba70",
+    "url": "/static/js/main.d2afc27e.chunk.js"
+  },
+  {
+    "revision": "9317f3805f694bb42e15",
+    "url": "/static/js/runtime-main.8ae07437.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
